8f77241d6901db955ee11d5adb341db8537550ee
Craig Jolley <jolleycraig@gmail.com>
2017-10-31 18:15:20 +0000

    Made some tweaks to my linear model. Still not happy with it, though.

--- lm.R
+++ lm.R
@@ -3,44 +3,69 @@
 library(dplyr)
 library(magrittr)
 
-source('empirical.R')
+# Empirical data only
 
-# Empirical data only, 10x cross-validation
-
+###############################################################################
+# Load training data, enrich with empirical averages
+###############################################################################
 train <- read.csv('train.csv') 
 
-items <- read.csv('items.csv') # need perishables for correct scoring
-train <- left_join(train,items %>% select(item_nbr,perishable),by='item_nbr')
 train <- train %>%
   mutate(promo_na=is.na(onpromotion),
          onpromotion=(!is.na(onpromotion) & onpromotion==TRUE),
          log_sales = ifelse(unit_sales >= 0,log(unit_sales+1),0))
 
-lm_xval <- function(train,fold) {
-  sapply(1:fold,function(i) {
-    train$test <- (train$id %% fold == i-1)
-    train_i <- augment(train)
-    test_i <- train_i %>% filter(test==TRUE) %>% 
-      select(-id,-date,-store_nbr,-unit_sales,-item_nbr,-test)
-    train_i <- train_i %>% filter(test==FALSE) %>% 
-      select(-id,-date,-store_nbr,-unit_sales,-item_nbr,-test)
-    my_lm <- lm(log_sales ~ .,data=train_i)
-    w <- ifelse(test_i$perishable==1,1.25,1)
-    score <- sqrt(sum(w*((predict(my_lm,test_i) - test_i$log_sales)^2)) / sum(w))
-    paste0(i,' ',score) %>% print
-    score
-  }) %>% mean
-}
+by_item <- train %>%
+  group_by(item_nbr) %>%
+  summarize(item_mean=mean(log_sales,na.rm=TRUE))
 
-n <- 1e5
-short <- train %>% 
-  filter(id %% round(nrow(train)/n) == 0)
-short$id <- 1:nrow(short)
+by_store <- train %>%
+  group_by(store_nbr) %>%
+  summarize(store_mean=mean(log_sales,na.rm=TRUE))
 
-lm_xval(short,10)
-# average of 0.8746726 when I run on my laptop with a subset of 10k rows
-# when I run without the itemstore columns, drops to 0.824913
-  
-# With the itemstore columns, I get the following on an AWS r4.2xlarge
-# 1e5 rows: 0.8020752
-  
+train_mean <- mean(train$log_sales)
+train_median <- median(train$log_sales)
+train_sd <- sd(train$log_sales)  
+
+j <- train %>% 
+  mutate(date=as.character(date)) %>%
+  left_join(by_item,by='item_nbr') %>%
+  left_join(by_store,by='store_nbr') %>%
+  select(-id,-date,-item_nbr,-store_nbr,-unit_sales)
+
+my_lm <- lm(log_sales ~ .,data=j)
+summary(my_lm)
+
+# This is actually sort of lame -- not much there.
+
+###############################################################################
+# Load test data, join averages from training data
+# Because test data contains some new items, may need to guess the appropriate
+# item_mean based on class and family.
+###############################################################################
+
+test <- read.csv('test.csv')
+
+items <- read.csv('items.csv') %>% 
+  left_join(by_item,by='item_nbr')
+
+by_class <- items %>%
+  group_by(class) %>%
+  summarize(class_mean=mean(item_mean,na.rm=TRUE))
+
+by_family <- items %>%
+  group_by(family) %>%
+  summarize(family_mean=mean(item_mean,na.rm=TRUE))
+
+items <- items %>%
+  left_join(by_class,by='class') %>%
+  left_join(by_family,by='family')
+
+j <- test %>%
+  left_join(items,by='item_nbr') %>% 
+  mutate(item_mean = ifelse(!is.na(item_mean),item_mean,
+                                    ifelse(!is.na(class_mean),class_mean,
+                                           family_mean)))
+
+# CHECK: what's the relationship between dates in the train and test datasets?
+# what about stores?
